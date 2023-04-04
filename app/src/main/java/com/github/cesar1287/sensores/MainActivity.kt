7cd25c62c8802b1e07620c0c30d1249bc9b5f10a
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-04-04 19:59:42 -0300

    shake detection

--- app/src/main/java/com/github/cesar1287/sensores/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/sensores/MainActivity.kt
@@ -2,17 +2,45 @@ package com.github.cesar1287.sensores
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.github.cesar1287.sensores.databinding.ActivityMainBinding
+import java.lang.Math.abs
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val sensorManager: SensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    private lateinit var tempSensor: Sensor
+
+    private lateinit var accelerometerSensor: Sensor
+
+    private var currentX = 0.0f
+    private var currentY = 0.0f
+    private var currentZ = 0.0f
+
+    private var lastX: Float = 0.0f
+    private var lastY: Float = 0.0f
+    private var lastZ: Float = 0.0f
+
+    private var xDifference: Float = 0.0f
+    private var yDifference: Float = 0.0f
+    private var zDifference: Float = 0.0f
+
+    private val shakeThreshold = 5.0f
+
+    private var itIsNotFirstTime = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +48,29 @@ class MainActivity : AppCompatActivity() {
 
         printSensors()
         specificSensor()
+
+//        if(sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) == null) {
+//            Log.i("SENSOR", "SENSOR DE TEMPERATURA INDISPONÍVEL")
+//        } else {
+//            tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+//        }
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) {
+            Log.i("SENSOR", "ACELEROMETRO INDISPONÍVEL")
+        } else {
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 
     private fun printSensors() {
@@ -39,5 +90,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun shakeDetection(it: SensorEvent) {
+        currentX = it.values[0]
+        currentY = it.values[1]
+        currentZ = it.values[2]
 
+        if (itIsNotFirstTime) {
+            xDifference = kotlin.math.abs(lastX - currentX)
+            yDifference = kotlin.math.abs(lastY - currentY)
+            zDifference = kotlin.math.abs(lastZ - currentZ)
+
+            if ((xDifference > shakeThreshold && yDifference > shakeThreshold) ||
+                (xDifference > shakeThreshold && zDifference > shakeThreshold) ||
+                (yDifference > shakeThreshold && zDifference > shakeThreshold)
+            ) {
+                Toast.makeText(this, "Shake", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        lastX = currentX
+        lastY = currentY
+        lastZ = currentZ
+        itIsNotFirstTime = true
+    }
+
+    override fun onSensorChanged(sensorEvent: SensorEvent?) {
+        sensorEvent?.let {
+            shakeDetection(it)
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
 }
