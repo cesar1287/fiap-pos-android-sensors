f59f03842866cfa812e1eaa377f8a0f6d4429ab5
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-04-04 19:36:09 -0300

    adding core methods

--- app/src/main/java/com/github/cesar1287/sensores/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/sensores/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.github.cesar1287.sensores
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.github.cesar1287.sensores.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +17,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        printSensors()
+        specificSensor()
     }
+
+    private fun printSensors() {
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensorList: List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
+        for(sensor in sensorList) {
+            Log.i("SENSOR: ", "Nome: ${sensor.name} - Tipo ${sensor.type} - ${sensor.stringType} ")
+        }
+    }
+
+    private fun specificSensor() {
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) == null) {
+            Log.i("SENSOR", "Device não possui sensor de temperatura")
+        } else {
+            Log.i("SENSOR", "Device possui sensor de temperatura")
+        }
+    }
+
+
 }
