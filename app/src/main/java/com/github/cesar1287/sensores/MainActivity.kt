5ea6f64b40267b36b69557206bad57c6af50c608
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-04-04 21:12:58 -0300

    added step counter

--- app/src/main/java/com/github/cesar1287/sensores/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/sensores/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.github.cesar1287.sensores
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.github.cesar1287.sensores.databinding.ActivityMainBinding
 import java.lang.Math.abs
 
@@ -27,6 +31,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var accelerometerSensor: Sensor
 
     private lateinit var gravitySensor: Sensor
+
+    private lateinit var stepCounter: Sensor
+    private lateinit var stepDetector: Sensor
+
+    private var stepDetect = 0
 
     private var currentX = 0.0f
     private var currentY = 0.0f
@@ -63,19 +72,35 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 //            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 //        }
 
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) == null) {
-            Log.i("SENSOR", "SENSOR DE GRAVIDADE INDISPONÍVEL")
-        } else {
-            gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
-        }
+//        if (sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) == null) {
+//            Log.i("SENSOR", "SENSOR DE GRAVIDADE INDISPONÍVEL")
+//        } else {
+//            gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
+//        }
 
+        checkPermissions()
     }
 
-//    override fun onResume() {
-//        super.onResume()
-//        sensorManager.registerListener(this, gravitySensor, SensorManager.SENSOR_DELAY_NORMAL)
-//    }
-//
+    private fun initSensors() {
+        initStepCounter()
+        initStepDetect()
+    }
+
+    private fun initStepCounter() {
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) == null) {
+            Log.i("SENSOR", "Sensor contador de passos indisponivel")
+        } else {
+            stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        }
+    }
+
+    private fun initStepDetect() {
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) == null) {
+            Log.i("SENSOR", "SENSOR detector de passos indisponivel")
+        } else {
+            stepDetector = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+        }
+    }
 
     override fun onResume() {
         super.onResume()
@@ -83,6 +108,22 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             sensorManager.registerListener(
                 this,
                 gravitySensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+
+        if (this::stepCounter.isInitialized) {
+            sensorManager.registerListener(
+                this,
+                stepCounter,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+
+        if (this::stepDetector.isInitialized) {
+            sensorManager.registerListener(
+                this,
+                stepDetector,
                 SensorManager.SENSOR_DELAY_NORMAL
             )
         }
@@ -110,6 +151,88 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
+    override fun onSensorChanged(sensorEvent: SensorEvent?) {
+        sensorEvent?.let {
+            //shakeDetection(it)
+            //flippedDetection(it)
+            when (it.sensor) {
+                stepCounter -> {
+                    binding.tvCounted.text = it.values[0].toString()
+                }
+                stepDetector -> {
+                    stepDetect += it.values[0].toInt()
+                    binding.tvDetected.text = stepDetect.toString()
+                }
+                gravitySensor -> {
+
+                }
+                accelerometerSensor -> {
+
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    private fun checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACTIVITY_RECOGNITION)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Verifica o status da permissão
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        android.Manifest.permission.ACTIVITY_RECOGNITION
+                    )
+                ) {
+                    // O usuário se recusa completamente a conceder permissão e geralmente solicita que o usuário entre na interface de configuração de permissão.
+                    // O usuário irá precisar entrar na interface de configuração de permissão para abrir
+                } else {
+                    //Solicitação do consentimento do usuario
+                    val permissions = listOf(android.Manifest.permission.ACTIVITY_RECOGNITION)
+                    ActivityCompat.requestPermissions(this, permissions.toTypedArray(), 1);
+                }
+            } else {
+                // Permissão concedida pelo usuário
+                initSensors()
+            }
+        } else {
+            initSensors()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            for (i in permissions.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    // Permissão concedida
+                    initSensors()
+                } else {
+                    // Permissão Negada
+                    // application failed
+                    Log.d("TAG", "[Permission]" + "ACTIVITY_RECOGNITION application failed")
+                }
+            }
+        }
+    }
+
+
+private fun flippedDetection(it: SensorEvent) {
+        if (it.values[2] < -9.7) {
+            Log.i("SENSOR", "Media paused")
+        } else {
+            Log.i("SENSOR", "Media started")
+        }
+    }
+
     private fun shakeDetection(it: SensorEvent) {
         currentX = it.values[0]
         currentY = it.values[1]
@@ -132,24 +255,5 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         lastY = currentY
         lastZ = currentZ
         itIsNotFirstTime = true
-    }
-
-    override fun onSensorChanged(sensorEvent: SensorEvent?) {
-        sensorEvent?.let {
-            shakeDetection(it)
-            //flippedDetection(it)
-        }
-    }
-
-    private fun flippedDetection(it: SensorEvent) {
-        if (it.values[2] < -9.7) {
-            Log.i("SENSOR", "Media paused")
-        } else {
-            Log.i("SENSOR", "Media started")
-        }
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-
     }
 }
